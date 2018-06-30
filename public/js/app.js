34042d0f96a372c0a96f308af3026129e8bc38c0
peachykeen5 <lexi.keen@gmail.com>
2018-06-30 20:46:44 +0000

    working with edit functions

--- public/js/app.js
+++ public/js/app.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
     let check = $('<input>', {type:'checkbox', class:'form-check-input', id:'exampleCheck1'});
     let item = $('<tt>').text(key.name)
     let edit = $('<button>').text('EDIT').addClass('btn btn-info btn-sm');
-    let deleteButton = $('<button>', {text: 'DELETE', id:"delete", class:'btn btn-info btn-sm'});
+    let deleteButton = $('<button>', {text: 'DELETE', id:"delete", class:'btn btn-info btn-sm', data:(item.id)});
     let $liItem = $('<li>', {class:"list-group-item", draggable:'true', ondragstart:"drag(event)"});
     $liItem.append(check, item, edit, deleteButton);
     $span.append($liItem);
@@ -40,29 +40,49 @@ $(document).ready(function () {
 
 });;
 
+ function deleteItems(list) {
+   $.ajax({
+     method: "POST",
+     url: "/",
+     success: function (list) {
+       loadItems(list);
+       console.log("list 2", list)
+     }
+   })
+ }
 
+ function totallyDeleteSomethingForRealsies(item){
+   return new Promise(function(resolve) {
+     setTimeout(resolve, 2000);
+   });
+ }
 //DELETE BUTTON
-
   $(document).on('click', '#delete.btn', function() {
-    $(this).parent().remove();
+    totallyDeleteSomethingForRealsies($(this).data('delete-id'))
+      .then(function(){
+        $(this).parent().remove();
+      })
 });
 
 
 
 // DRAG AND DROP FUNCTIONALITY
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-}
+//function allowDrop(ev) {
+//  ev.preventDefault();
+//}
+//
+//function drag(ev) {
+//  ev.dataTransfer.setData("text", ev.target.id);
+//  console.log(ev)
+//}
+//
+//function drop(ev) {
+//  ev.preventDefault();
+//  var data = ev.dataTransfer.getData("text");
+//  console.log('Data: ', data);//nothing 
+//  console.log('document', document.getElementById(data));//null
+//  ev.target.appendChild(document.getElementById(data));
+//}
 
 
 
